479840d17f9cd6c82f1bf1ac0b42949318b283a1
thefx3 <fx.pairault@yahoo.fr>
2025-01-21 17:40:38 +0100

    Tests to 5

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,25 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array, ...args) {
+
+
+//     let position = thearray.indexOf(num);
+//     let sliced = thearray.toSpliced(position,1);
+
+//     while (sliced.includes(num)==true){
+//       position = sliced.indexOf(num);
+//       sliced = sliced.toSpliced(position,1);
+//    } 
+
+//     console.log(sliced);
+//     return sliced;
+
+    const newArray = [];
+    array.forEach((item) => {  // `item` is a parameter here
+      if (!args.includes(item)) {
+        newArray.push(item);
+      }
+    });
+    return newArray;
+  };
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,31 @@
-const sumAll = function() {
+const sumAll = function(int1,int2) {
 
+    let sum = 0;
+    let min = 0;
+    let max = 0;
+
+
+    //Preconditions
+    if (!Number.isInteger(int1) || !Number.isInteger(int2)) return "ERROR";
+    if (int1 < 0 || int2 < 0) return "ERROR";
+    
+    if (int1 < int2){
+        min = int1;
+        max = int2;
+    } else if (int1 > int2){
+        min = int2;
+        max = int1;
+    } else if (int1==int2){
+        return sum = int1+int2;
+    }
+
+    for (;min<max+1;min++){
+        sum = sum + min;
+    }
+    console.log(sum);
+    return sum;
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
