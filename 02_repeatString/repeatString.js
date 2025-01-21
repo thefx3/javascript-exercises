f6bfb5a67bfda4ea62f846b0ab94b7993be84fc8
thefx3 <fx.pairault@yahoo.fr>
2025-01-21 11:03:11 +0100

    Test2 done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string,num) {
+    let finalstring = "";
 
+    if (num<0){
+        return 'ERROR';
+    }
+
+    for (let i=0; i<num;i++){
+        finalstring = finalstring + string;
+    }
+
+    return finalstring;
 };
 
 // Do not edit below this line
