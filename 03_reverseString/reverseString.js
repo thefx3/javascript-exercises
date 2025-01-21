f6725687a1ac841428a94c61d9778396a5133f5c
thefx3 <fx.pairault@yahoo.fr>
2025-01-21 12:48:52 +0100

    Test3 done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,16 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    const stringlength = string.length;
+    let stringreversed = "";
 
-};
+
+for (let i = stringlength-1;i>=0; i--){
+    stringreversed = stringreversed + string.at(i);
+}
+    return stringreversed;
+}
+
+reverseString("Hello");
+//reverseString("AbcDefghijklmnopqrstuvWxYZ");
 
 // Do not edit below this line
 module.exports = reverseString;
