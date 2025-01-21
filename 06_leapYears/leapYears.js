373061c082ec355a6e4f09b08a1f6dce8e1eeaa8
thefx3 <fx.pairault@yahoo.fr>
2025-01-21 17:57:12 +0100

    Lap years ok

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,17 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+    console.log(year%4, year%100, year%400);
+
+    if (year%4==0){
+        if (year%100==0){
+            if (year%400==0){
+                return true;
+            }
+            return false;
+        } 
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
