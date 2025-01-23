5b23dcdd930b6ef81a1213d948cf8df3be9b19a6
thefx3 <fx.pairault@yahoo.fr>
2025-01-23 16:45:27 +0100

    Test temp ok

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,23 @@
-const convertToCelsius = function() {
-};
+const convertToCelsius = function(tempF) { // 0 --> 32
 
-const convertToFahrenheit = function() {
-};
+  let tempC = ((tempF-32)*(5/9));
+  console.log(Math.round(tempC*10)/10);
+
+  return Math.round(tempC*10)/10;
+
+}
+
+const convertToFahrenheit = function(tempC) { //32 --> 0
+
+  let tempF = (tempC*(9/5)+32);
+  console.log(Math.round(tempF*10)/10);
+
+  return Math.round(tempF*10)/10;
+
+}
+
+convertToCelsius(100);
+convertToFahrenheit(0);
 
 // Do not edit below this line
 module.exports = {
